2f3763f5207a0a1601d22c9d8442b98caf264e8a
Rocco Francesco Basta <rfrancesco.basta@gmail.com>
2026-06-09 15:53:44 +0200

    Models/Book: make User nullable, initialize strings to empty

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,11 +2,11 @@ namespace LibraryManager
 {
     public class Book {
         public int Id { get; set; }
-        public string Author { get; set; }  // Could be split 
-        public string Title { get; set; }
-        public string Genre { get; set; }
+        public string Author { get; set; } = string.Empty;  // Could be split 
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
         public int? BorrowedByUserId { get; set; }
-        public User BorrowedBy { get; set; }
+        public User? BorrowedBy { get; set; }
 
 
     }
