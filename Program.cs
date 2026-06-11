eecfcc4f129a82db269c18ee665d676a2853313b
Rocco Francesco Basta <rfrancesco.basta@gmail.com>
2026-06-11 17:41:57 +0200

    Use Swagger for API documentation

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace LibraryManager
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=library.db"));
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
 
             using (var scope = app.Services.CreateScope())
@@ -16,6 +18,14 @@ namespace LibraryManager
                 var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 DbInitializer.InitializeIfEmpty(dbContext);
             }
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
 
             BooksEndpoint.Map(app);
             AuthorsEndpoint.Map(app);
