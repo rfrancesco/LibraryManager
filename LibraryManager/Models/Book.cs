c7bced941a68fc012ad2d8580ed1aa1c9f9c0add
Rocco Francesco Basta <rfrancesco.basta@gmail.com>
2026-07-28 23:09:22 +0200

    Model: Add MaxLength to text fields

--- LibraryManager/Models/Book.cs
+++ LibraryManager/Models/Book.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryManager
 {
     public class Book
     {
         public int BookId { get; set; }
+        [MaxLength(100)]
         public string Author { get; set; } = string.Empty;  // Could be split 
+        [MaxLength(100)]
         public string Title { get; set; } = string.Empty;
+        [MaxLength(100)]
         public string Genre { get; set; } = string.Empty;
         public ICollection<Loan> Loans { get; set; } = [];
     }
--- LibraryManager/Models/User.cs
+++ LibraryManager/Models/User.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryManager
 {
     public class User
     {
         public int UserId { get; set; }
+        [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
         public ICollection<Loan> Loans { get; set; } = [];
     }
